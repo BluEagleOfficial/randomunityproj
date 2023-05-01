0a8954c68907c04ee9ae1652cd97a50829b2b7a9
BluEagle <soimstefan@gmail.com>
2023-05-01 18:32:19 +0300

    boat selector (a bit broken)

---
+++ collabproj1try/Assets/Scripts/BoatDetect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BoatDetect : MonoBehaviour
+{
+    private GameObject detectedObj;
+    [SerializeField] private MenuManager mm;
+    [SerializeField] private KeyCode interactionButton;
+    [SerializeField] private GameObject buttonPrompt;
+    [SerializeField] private GameObject eventObj;
+    [SerializeField] private GameObject title;
+    
+    private void OnTriggerStay(Collider col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            detectedObj = col.gameObject;
+            // Debug.Log("boat entered: " + detectedObj);
+            buttonPrompt.SetActive(true);
+            title.SetActive(true);
+            eventObj.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            detectedObj = null;
+            // Debug.Log("boat exited: " + detectedObj);
+            buttonPrompt.SetActive(false);
+            title.SetActive(false);
+            eventObj.SetActive(false);
+        }
+    }
+}
