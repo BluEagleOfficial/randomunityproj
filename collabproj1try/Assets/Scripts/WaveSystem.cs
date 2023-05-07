109cda91cc1801191978a26e0f2e5a7e69e73547
BluEagle <soimstefan@gmail.com>
2023-05-07 17:13:48 +0300

    waewafe

---
+++ collabproj1try/Assets/Scripts/WaveSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSystem : MonoBehaviour
+{
+    public int currentWave;
+    public int maxWaves;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
