1bea22a40553ba1a16e631d1a9a7a13bd72a7eac
BluEagle <soimstefan@gmail.com>
2023-05-25 19:08:58 +0300

    settings

--- collabproj1try/Assets/Scripts/settingsManager.cs
+++ collabproj1try/Assets/Scripts/settingsManager.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 
 public class settingsManager : MonoBehaviour
 {
+    [Header("Components")]
     private PlayerHud ph;
     [SerializeField] private AudioMixer soundMixer;
     [SerializeField] private AudioMixer musicMixer;
@@ -15,12 +16,12 @@ public class settingsManager : MonoBehaviour
     [SerializeField]
     UniversalRenderPipelineAsset[] renderAssets;
 
+    [Space]
+    [Header("Variables")]
     public bool vsync;
     public bool fullscreen = true;
     // public bool motionBlur;
     // [Tooltip("enable to show hp numbers on healthbar")] public bool showNumbers;
-
-    // public int musicVolume;
     public int SoundVolume = 100;
     public int MusicVolume = 100;
     public int fpsLimit = 60;
@@ -29,21 +30,29 @@ public class settingsManager : MonoBehaviour
     public int fov;
     public int resolutionScale = 100;
 
-    // UI
+    [Space]
+    [Header("UI Elements")]
     public TMP_Dropdown resolutionDropdown;
-
     public Toggle vsyncToggle;
     public Toggle fullscreenToggle;
     // public Toggle motionBlurToggle;
     // public Toggle showNumbersToggle;
-
     public Slider soundSlider;
     public Slider musicSlider;
     public Slider fpsLimitSlider;
     public TMP_Dropdown graphicsQualityDropdown;
+    public Slider resolutionScaleSlider;
     public Slider sensitivitySlider;
     public Slider fovSlider;
-    public Slider resolutionScaleSlider;
+
+    [Space]
+    [Header("Display Values")]
+    [SerializeField] private TMP_Text soundText;
+    [SerializeField] private TMP_Text musicText;
+    [SerializeField] private TMP_Text fpsLimitText;
+    [SerializeField] private TMP_Text resolutionScaleText;
+    [SerializeField] private TMP_Text sensitivityText;
+    [SerializeField] private TMP_Text fovText;
 
     void Start()
     {
@@ -74,6 +83,7 @@ public class settingsManager : MonoBehaviour
     public void changeSoundVolume()
     {
         SoundVolume = (int)soundSlider.value;
+        soundText.text = Mathf.Abs(SoundVolume * 5).ToString() + "%";
         soundMixer.SetFloat("Sound", SoundVolume);
         if(soundSlider.value <= -20)
             soundMixer.SetFloat("Sound", -80);
@@ -82,6 +92,7 @@ public class settingsManager : MonoBehaviour
     public void changeMusicVolume()
     {
         MusicVolume = (int)musicSlider.value;
+        musicText.text = Mathf.Abs(musicSlider.value * 5).ToString() + "%";
         musicMixer.SetFloat("Music", MusicVolume);
         if(musicSlider.value <= -20)
             musicMixer.SetFloat("Music", -80);
@@ -111,27 +122,34 @@ public class settingsManager : MonoBehaviour
 
     public void changeFpsLimit()
     {
-        fpsLimit = (int)fpsLimitSlider.value + 30; // as explained below, compensating
-        Mathf.Clamp(fpsLimit, 30, 240);
+        fpsLimit = (int)fpsLimitSlider.value;
+        fpsLimitText.text = (fpsLimitSlider.value).ToString() + " fps"; 
+        // Mathf.Clamp(fpsLimit, 30, 240);
         Debug.Log("fps limit is " + fpsLimit);
+        if(fpsLimitSlider.value >= 240)
+            Debug.Log("unlimited fps");
+            
     }
 
     public void changeSensitivity()
     {
         sensitivity = (int)sensitivitySlider.value;
+        sensitivityText.text = sensitivitySlider.value.ToString();
         Debug.Log("sensitivity is " + sensitivity);
     }
 
     public void changeFov()
     {
         fov = (int)fovSlider.value;
+        fovText.text = fovSlider.value.ToString();
         Debug.Log("field of view is " + fov);
     }
 
     public void changeResolutionScale()
     {
-        resolutionScale = (int)resolutionScaleSlider.value + 25; // since the min value of the slider is not equal to min scale value, compensating
-        Mathf.Clamp(resolutionScale, 30, 200);
+        resolutionScale = (int)resolutionScaleSlider.value;
+        resolutionScaleText.text = (resolutionScaleSlider.value).ToString();
+        // Mathf.Clamp(resolutionScale, 25, 200);
 
         foreach (UniversalRenderPipelineAsset asset in renderAssets)
         {
