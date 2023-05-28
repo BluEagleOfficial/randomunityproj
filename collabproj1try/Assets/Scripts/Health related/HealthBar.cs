2f1fe04026613a60045c570974d89c74c65b88b7
BluEagle <soimstefan@gmail.com>
2023-05-28 16:59:13 +0300

    hp text fix

--- collabproj1try/Assets/Scripts/Health related/HealthBar.cs
+++ collabproj1try/Assets/Scripts/Health related/HealthBar.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Health hp;
     [SerializeField] private Slider s;
-    [SerializeField] private TMP_Text t;
 
     void Update()
     {
         s.maxValue = hp.maxhp;
         s.value = hp.hp;
-        t.text = hp.hp.ToString() + " Hp";
     }
 }
