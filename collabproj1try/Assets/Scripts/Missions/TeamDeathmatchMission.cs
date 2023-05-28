5dd0a6be877edcae0129fc84be5fda35226c7a72
BluEagle <soimstefan@gmail.com>
2023-05-28 15:37:46 +0300

    team deathmatch

---
+++ collabproj1try/Assets/Scripts/Missions/TeamDeathmatchMission.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "TeamDeathmatchMission", menuName = "Missions/TeamDeathmatchMission")]
+public class TeamDeathmatchMission : MissionBase
+{
+
+    public float timeLeft = 900;
+
+    public int howManyEnemies = 5;
+    public int howManyAllies = 4;
+
+    public Vector3 minEnemyRange = new Vector3(-200,0,-200), maxEnemyRange = new Vector3(200,0,0);
+    public Vector3 minAllyRange = new Vector3(-200,0,200), maxAllyRange = new Vector3(200,0,0);
+    public List<GameObject> enemyPrefabs = new List<GameObject>();
+    public List<GameObject> allyPrefabs = new List<GameObject>();
+
+    public GameObject[] enemies;
+
+    public override void StartMission(GameManager gm)
+    {
+        for (int i = 0; i < howManyEnemies; i++)
+        {
+            int randomNumber = Random.Range(0,2);
+            Vector3 pos = new Vector3(Random.Range(minEnemyRange.x, maxEnemyRange.x), Random.Range(minEnemyRange.y, maxEnemyRange.y), Random.Range(minEnemyRange.z, maxEnemyRange.z));
+            Instantiate(enemyPrefabs[randomNumber], pos, Quaternion.identity);
+        }
+
+        for (int i = 0; i < howManyAllies; i++)
+        {
+            int randomNumber = Random.Range(0,2);
+            Vector3 pos = new Vector3(Random.Range(minAllyRange.x, maxAllyRange.x), Random.Range(minAllyRange.y, maxAllyRange.y), Random.Range(minAllyRange.z, maxAllyRange.z));
+            Instantiate(allyPrefabs[randomNumber], pos, Quaternion.identity);
+        }
+    }
+    public override void UpdateMission(GameManager gm)
+    {
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+
+        timer += Time.deltaTime;
+        if (timer > timeLeft)
+        {
+            if (enemies.Length <= 0 && gm.playerHealth.dead == false)
+            {
+                win = true;
+            }
+        }
+    }
+    public override void EndMission(GameManager gm)
+    {
+
+    }
+    public override void resetData()
+    {
+        win = false;
+        timer = 0;
+    }
+}
